874ba8ea4b6a165f25cfb5c378491eeaae20f47d
zhongwen <dengzhongwen@eryuapp.com>
2015-11-06 00:54:28 +0800

    some synatx warning by make elvis

--- src/observer_cli_table.erl
+++ src/observer_cli_table.erl
@@ -21,16 +21,16 @@ draw_ets_info() ->
   end, AllEtsInfo),
   io:format("|\e[46m~-24.24s|~-12.12s|~-12.12s|~-12.12s|~-10.10s|~6.6s|~-24.24s|~-12.12s|~-11.11s\e[49m|~n",
     ["id or name", "memory", "size", "type", "protection",
-      "keypos","write/read concurrency", "owner", "named_table"]),
+      "keypos", "write/read concurrency", "owner", "named_table"]),
   [begin
      Name = get_value(name, Ets), Memory = get_value(memory, Ets),
      Size = get_value(size, Ets), Type = get_value(type, Ets),
      Protect = get_value(protection, Ets), KeyPos = get_value(keypos, Ets),
      Write = get_value(write_concurrency, Ets), Read = get_value(read_concurrency, Ets),
      Owner = get_value(owner, Ets), NamedTable = get_value(named_table, Ets),
-     IdOrName = case is_atom(Id) of true -> to_list(Id); false -> to_list(Id) ++"/" ++ Name end,
+     IdOrName = case is_atom(Id) of true -> to_list(Id); false -> to_list(Id) ++ "/" ++ Name end,
      io:format("|~-24.24s|~-12.12s|~-12.12s|~-12.12s|~-10.10s|~6.6s|~-24.24s|~-12.12s|~10.10s |~n",
-     [IdOrName, Memory, Size, Type, Protect, KeyPos, Write ++"/" ++ Read, Owner, NamedTable])
+     [IdOrName, Memory, Size, Type, Protect, KeyPos, Write ++ "/" ++ Read, Owner, NamedTable])
    end||{Id, Ets} <- lists:sublist(SorEtsInfo, ?MAX_SHOW_LEN)].
 
 get_value(Key, List) ->
@@ -43,19 +43,19 @@ to_list(Val) -> Val.
 
 get_ets_info(Tab) ->
   case catch ets:info(Tab) of
-    {'EXIT', _} -> {Tab, [{name, unread},%%it maybe die
+    {'EXIT', _} -> {Tab, [{name, unread}, %%it maybe die
       {write_concurrency, unread},
       {read_concurrency, unread},
       {compressed, unread},
       {memory, 0 },
-      {owner,unread},
-      {heir,unread},
-      {size,unread},
-      {node,unread},
-      {named_table,unread},
-      {type,unread},
-      {keypos,unread},
-      {protection,unread}]};
+      {owner, unread},
+      {heir, unread},
+      {size, unread},
+      {node, unread},
+      {named_table, unread},
+      {type, unread},
+      {keypos, unread},
+      {protection, unread}]};
     Infos when is_list(Infos)->
       Owner = proplists:get_value(owner, Infos),
       case is_reg(Owner) of
