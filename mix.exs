9142851a91b9f314b1bee60b94fd2e2c3fb419bf
Michael Klishin <michael@clojurewerkz.org>
2019-05-14 10:55:28 +0300

    Include a minimal mix.exs build file (#57)
    * Include a minimal mix.exs build file
    
    So that this project can be compiled by Mix as part of an Elixir
    codebase.
    
    Pair: @dumbbell.
    
    * .app.src file: list files for Hex.pm publishing
    
    While at it, remove a deprecated field.
    
    Pair: @dumbbell.
    
    * mix.exs: specify that project language is Erlang
    
    * Correct version number in mix.exs

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule ObserverCli.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :observer_cli,
+      version: "1.4.4",
+      language: :erlang,
+      start_permanent: Mix.env == :prod,
+      deps: []
+    ]
+  end
+
+  def application do
+    []
+  end
+end
