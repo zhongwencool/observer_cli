9685c97c2933fca4515ea8e3b40ef89de6125d31
zhongwencool <zhongwencool@gmail.com>
2021-02-25 11:12:52 +0800

    remove precise opt version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ObserverCli.MixProject do
   def project do
     [
       app: :observer_cli,
-      version: "1.6.0",
+      version: "1.6.1",
       language: :erlang,
       description: "observer in shell",
       deps: [
