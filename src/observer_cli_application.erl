c14382e3123cd3caa43208a60ae06931cd411512
zhongwencool <zhongwencool@gmail.com>
2021-08-25 10:11:38 +0800

    application add status: started/loaded/loading etc

--- src/observer_cli_application.erl
+++ src/observer_cli_application.erl
@@ -36,16 +36,16 @@ manager(Pid, Opts = #view_opts{app = App = #app{cur_page = CurPage}}) ->
             quit;
         {func, proc_count, reductions} ->
             clean([Pid]),
-            start(Opts#view_opts{app = App#app{type = {reductions, 1}}});
+            start(Opts#view_opts{app = App#app{type = {reductions, 2}}});
         {func, proc_count, memory} ->
             clean([Pid]),
-            start(Opts#view_opts{app = App#app{type = {memory, 2}}});
+            start(Opts#view_opts{app = App#app{type = {memory, 3}}});
         {func, proc_count, message_queue_len} ->
             clean([Pid]),
-            start(Opts#view_opts{app = App#app{type = {message_queue_len, 3}}});
+            start(Opts#view_opts{app = App#app{type = {message_queue_len, 4}}});
         pause_or_resume ->
             clean([Pid]),
-            start(Opts#view_opts{app = App#app{type = {process_count, 4}}});
+            start(Opts#view_opts{app = App#app{type = {process_count, 5}}});
         {new_interval, NewInterval} ->
             clean([Pid]),
             start(Opts#view_opts{app = App#app{interval = NewInterval}});
@@ -80,9 +80,9 @@ render_worker(App, AutoRow) ->
 render_app_info(Row, CurPage, {Type, N}) ->
     List = [
         begin
-            {0, element(N, I), [App, C, R, M, Q, V]}
+            {0, element(N, I), [App, C, R, M, Q, V, S]}
         end
-        || {{App, V}, I = {R, M, Q, C}} <- maps:to_list(app_info())
+        || {{App, V}, I = {S, R, M, Q, C}} <- maps:to_list(app_info())
     ],
     {StartPos, SortList} = observer_cli_lib:sublist(List, Row, CurPage),
     InitColor = [
@@ -107,24 +107,27 @@ render_app_info(Row, CurPage, {Type, N}) ->
         ?UNDERLINE,
         ?W2(MemColor, "Memory(m)", 20),
         ?UNDERLINE,
-        ?W2(RedColor, "Reductions(r)", 20),
+        ?W2(RedColor, "Reductions(r)", 17),
         ?UNDERLINE,
-        ?W2(MsgQColor, "MsgQ(mq)", 15),
+        ?W2(MsgQColor, "MsgQ(mq)", 10),
         ?UNDERLINE,
-        ?W2(?GRAY_BG, "version", 21)
+        ?W2(?GRAY_BG, "Status", 12),
+        ?UNDERLINE,
+        ?W2(?GRAY_BG, "version", 16)
     ]),
     {_, View} = lists:foldl(
         fun({_, _, Item}, {Pos, Acc}) ->
-            [App, C, R, M, Q, V] = Item,
+            [App, C, R, M, Q, V, S] = Item,
             {Pos + 1, [
                 ?render([
                     ?W(Pos, 2),
                     ?W(App, 29),
                     ?W(C, 18),
                     ?W({byte, M}, 18),
-                    ?W(R, 18),
-                    ?W(Q, 13),
-                    ?W(V, 20)
+                    ?W(R, 15),
+                    ?W(Q, 8),
+                    ?W(S, 10),
+                    ?W(V, 15)
                 ])
                 | Acc
             ]}
@@ -140,6 +143,10 @@ app_info() ->
     Info = application:info(),
     {running, Running} = lists:keyfind(running, 1, Info),
     {loaded, Loaded} = lists:keyfind(loaded, 1, Info),
+    {loading, Loading} = lists:keyfind(loading, 1, Info),
+    {started, Started} = lists:keyfind(started, 1, Info),
+    {start_p_false, StartPFalse} = lists:keyfind(start_p_false, 1, Info),
+    {starting, Starting} = lists:keyfind(starting, 1, Info),
     Leaders =
         lists:foldl(
             fun
@@ -157,6 +164,21 @@ app_info() ->
             #{},
             Running
         ),
+    Status = lists:foldl(fun({App, _desc, Version}, Acc) ->
+        IsLoading = lists:keyfind(App, 1, Loading) =/= false,
+        IsStarting = lists:keyfind(App, 1, Starting) =/= false,
+        IsStartPFalse = lists:keyfind(App, 1, StartPFalse) =/= false,
+        IsStarted = lists:keyfind(App, 1, Started) =/= false,
+        IsLoaded = lists:keyfind(App, 1, Loaded) =/= false,
+        case {IsLoading, IsStarting, IsStartPFalse, IsStarted, IsLoaded} of
+            {true, _, _, _, _} -> Acc#{{App, Version} => "Loading"};
+            {_, true, _, _, _} -> Acc#{{App, Version} => "Starting"};
+            {_, _, true, _, _} -> Acc#{{App, Version} => "StartPFalse"};
+            {_, _, _, true, _} -> Acc#{{App, Version} => "Started"};
+            {_, _, _, _, true} -> Acc#{{App, Version} => "Loaded"};
+            _ -> Acc#{{App, Version} => "Unknown"}
+        end
+                         end, #{}, application:which_applications(5000)),
     lists:foldl(
         fun(Pid, Acc) ->
             case erlang:process_info(Pid, [group_leader, memory, reductions, message_queue_len]) of
@@ -171,19 +193,20 @@ app_info() ->
                     ] = Prop,
                     case maps:find(Group, Leaders) of
                         error ->
-                            {ok, {R1, M1, Q1, C1}} = maps:find(?Unknown, Acc),
-                            NewInfo = {R1 + Reds, M1 + Memory, Q1 + MsgQ, C1 + 1},
+                            {ok, {S1, R1, M1, Q1, C1}} = maps:find(?Unknown, Acc),
+                            NewInfo = {S1, R1 + Reds, M1 + Memory, Q1 + MsgQ, C1 + 1},
                             maps:put(?Unknown, NewInfo, Acc);
                         {ok, AppInfo} ->
                             case maps:find(AppInfo, Acc) of
-                                {ok, {R, M, Q, C}} ->
-                                    maps:put(AppInfo, {R + Reds, M + Memory, Q + MsgQ, C + 1}, Acc);
+                                {ok, {S, R, M, Q, C}} ->
+                                    maps:put(AppInfo, {S, R + Reds, M + Memory, Q + MsgQ, C + 1}, Acc);
                                 error ->
-                                    maps:put(AppInfo, {Reds, Memory, MsgQ, 1}, Acc)
+                                    {ok, S} = maps:find(AppInfo, Status),
+                                    maps:put(AppInfo, {S, Reds, Memory, MsgQ, 1}, Acc)
                             end
                     end
             end
         end,
-        #{?Unknown => {0, 0, 0, 0}},
+        #{?Unknown => {"unknown", 0, 0, 0, 0}},
         erlang:processes()
     ).
